2c950cb935190caf57e1af2152bc99e0e0d434a8
Yves <robertgherghe0103@gmail.com>
2022-12-29 16:57:44 +0200

    Adding Images + Changing Form1

---
+++ MgmtSpitalWF/ModulPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MgmtSpitalWF
+{
+    partial class ModulPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSlide = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.panelSlide.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSlide
+            // 
+            this.panelSlide.Controls.Add(this.panelLogo);
+            this.panelSlide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSlide.Location = new System.Drawing.Point(0, 0);
+            this.panelSlide.Name = "panelSlide";
+            this.panelSlide.Size = new System.Drawing.Size(200, 661);
+            this.panelSlide.TabIndex = 0;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 100);
+            this.panelLogo.TabIndex = 1;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(200, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(984, 20);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // ModulPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelSlide);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ModulPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MgmtSpital";
+            this.panelSlide.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSlide;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitle;
+    }
+}
+
--- MgmtSpitalWF/ModulPrincipal.cs
+++ MgmtSpitalWF/ModulPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MgmtSpitalWF
 {
-    public partial class Form1 : Form
+    public partial class ModulPrincipal : Form
     {
-        public Form1()
+        public ModulPrincipal()
         {
             InitializeComponent();
         }
